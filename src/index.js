5e732769f6ca6d3a1632ea18e232d2a887b66266
osamaashraf6 <osamaashraf668@gmail.com>
2022-12-05 22:29:41 +0200

    Add the todoContainer js to the render

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
-import App from './App';
+import TodoContainer from './components/TodoContainer';
 import './App.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TodoContainer />
   </React.StrictMode>,
   document.getElementById('root'),
 );
