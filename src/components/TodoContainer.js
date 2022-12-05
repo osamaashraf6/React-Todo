9ef3cdfa2e119394907aa1658fc8183861a0a708
osamaashraf6 <osamaashraf668@gmail.com>
2022-12-05 22:30:22 +0200

    Add the components of the todos

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import TodosList from './TodosList';
+import Header from './Header';
+import InputTodo from './InputTodo';
+
+class TodoContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          title: 'Setup development environment',
+          completed: true,
+        },
+        {
+          id: 2,
+          title: 'Develop website and add content',
+          completed: false,
+        },
+        {
+          id: 3,
+          title: 'Deploy to live server',
+          completed: false,
+        },
+      ],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      .then((response) => response.json())
+      .then((data) => this.setState({ todos: data }));
+  }
+
+  handleChange = (id) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      }),
+    }));
+  };
+
+  handleRemove = (id) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.filter((item) => item.id !== id),
+    }));
+  };
+
+  handleAddTodo = (text) => {
+    const { todos } = this.state;
+    const newTodo = {
+      id: todos.length,
+      title: text,
+      completed: false,
+    };
+    this.setState({ todos: [...todos, newTodo] });
+  };
+
+  setUpdate = (updatedTitle, id) => {
+    const { todos } = this.state;
+    this.setState({
+      todos: todos.map((todo) => {
+        if (todo.id === id) {
+          // eslint-disable-next-line no-param-reassign
+          todo.title = updatedTitle;
+        }
+        return todo;
+      }),
+    });
+  };
+
+  render() {
+    const { todos } = this.state;
+    return (
+      <div className="container">
+        <div className="inner">
+          <Header />
+          <InputTodo handleAddTodo={this.handleAddTodo} />
+          <TodosList
+            handleRemove={this.handleRemove}
+            handleChange={this.handleChange}
+            todos={todos}
+            setUpdate={this.setUpdate}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+export default TodoContainer;
