542c36952707320058c2fe2b9c36bf23286bbe05
osamaashraf6 <osamaashraf668@gmail.com>
2022-12-06 17:33:39 +0200

    Add the required changes, I replaced the render with create root to fix the console error as the reviwer requested from me

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import TodoContainer from './components/TodoContainer';
 import './App.css';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <TodoContainer />
   </React.StrictMode>,
-  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
